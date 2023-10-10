c6bf0ddd6f1658f673a65cdca68d8aab2de77905
RaMadaSilva <raul01mateia@gmail.com>
2023-10-10 16:44:50 +0100

    ValueObject Email

--- JWTStore.Core/AcountContext/ValeuObjects/Email.cs
+++ JWTStore.Core/AcountContext/ValeuObjects/Email.cs
@@ -1,16 +1,40 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using JWTStore.Core.SharedContext.Extentions;
 using JWTStore.Core.SharedContext.ValueObjects;
 
 namespace JWTStore.Core.AcountContext.ValeuObjects
 {
-    public class Email : ValeuObject
+    public partial class Email : ValeuObject
     {
-        private const string Pattern = @"/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/";
+        private const string Pattern = @"/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/";
+        public Email(string Adress)
+        {
+            if (string.IsNullOrEmpty(Adress))
+                throw new Exception("E-mail invalido");
+
+            Adress = Adress.Trim().ToLower();
+
+            if (Adress.Length < 5)
+                throw new Exception("E-mail Invalido");
+
+            if (!EmailRegex().IsMatch(Adress))
+                throw new Exception("E-mail Invalido");
+
+        }
         public string Adress { get; }
         public string Hash => Adress.ToBase64();
+
+        [GeneratedRegex(Pattern)]
+        private static partial Regex EmailRegex();
+
+        public static implicit operator String(Email email)
+            => email.ToString();
+
+        public static implicit operator Email(string Adress)
+            => new Email(Adress);
+
     }
 }
