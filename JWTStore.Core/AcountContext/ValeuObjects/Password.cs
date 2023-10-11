6ddf5a6f444713775b7aafb78e0cbf4ae51988eb
RaMadaSilva <raul01mateia@gmail.com>
2023-10-11 01:35:00 +0100

    Gerando password aleatoria

--- JWTStore.Core/AcountContext/ValeuObjects/Password.cs
+++ JWTStore.Core/AcountContext/ValeuObjects/Password.cs
@@ -11,8 +11,22 @@ namespace JWTStore.Core.AcountContext.ValeuObjects
         private const string Valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ";
         private const string Special = "!@#$%^&*(){}[];";
 
-        public string Hash{ get;} = string.Empty; 
-        public string ResetCode { get; } = Guid.NewGuid().ToString("N")[..8].ToUpper(); 
+        public string Hash { get; } = string.Empty;
+        public string ResetCode { get; } = Guid.NewGuid().ToString("N")[..8].ToUpper();
+
+        private static string Generator(short length = 16, bool includeSpecialChars = true, bool upperCase = false)
+        {
+            var chars = includeSpecialChars ? (Valid + Special) : Valid;
+            var starRandom = upperCase ? 26 : 0;
+            var index = 0;
+            var res = new char[length];
+            var rnd = new Random();
+
+            while (index < length)
+                res[index++] = chars[rnd.Next(starRandom, chars.Length)];
+
+            return new string(res);
+        }
 
     }
 }
